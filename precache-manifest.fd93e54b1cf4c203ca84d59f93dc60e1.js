cd07abe12ba6fdd617be511c52923559fbb0a008
I320982 <yogita.gurnani@sap.com>
2020-08-31 18:50:32 +0530

    Updates

---
+++ precache-manifest.fd93e54b1cf4c203ca84d59f93dc60e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd254a5098c09aee3e363d8f9c492eb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984fd23f0f66db376c1b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0d5449dc8e242ebc635c",
+    "url": "/static/css/main.446f9144.chunk.css"
+  },
+  {
+    "revision": "984fd23f0f66db376c1b",
+    "url": "/static/js/2.5dc4ee77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5dc4ee77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5449dc8e242ebc635c",
+    "url": "/static/js/main.b4888bbf.chunk.js"
+  },
+  {
+    "revision": "14a926e5a3ab9b0224ac",
+    "url": "/static/js/runtime-main.62527498.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MobileStore/precache-manifest.bd74147e06dce73ecf1130a9c7dc105b.js"
+  "/precache-manifest.fd93e54b1cf4c203ca84d59f93dc60e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MobileStore/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
